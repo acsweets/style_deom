ac0b6e471ee89afbd1a9770b86982aa66c539f52
liuxiaocao <liuxiaocao@wehanyu.com>
2023-03-24 19:20:21 +0800

    fix

--- middle/src/main/java/com/example/middle/RelativeCodeActivity.java
+++ middle/src/main/java/com/example/middle/RelativeCodeActivity.java
@@ -75,13 +75,11 @@ public class RelativeCodeActivity extends AppCompatActivity implements OnClickLi
         // 给该视图设置布局参数
         v.setLayoutParams(rl_params);
         // 设置该视图的长按监听器
-        v.setOnLongClickListener(new OnLongClickListener() {
-            // 在用户长按该视图时触发
-            public boolean onLongClick(View vv) {
-                // 一旦监听到长按事件，就从相对布局中删除该视图
-                rl_content.removeView(vv);
-                return true;
-            }
+        // 在用户长按该视图时触发
+        v.setOnLongClickListener(vv -> {
+            // 一旦监听到长按事件，就从相对布局中删除该视图
+            rl_content.removeView(vv);
+            return true;
         });
         // 往相对布局中添加该视图
         rl_content.addView(v);
